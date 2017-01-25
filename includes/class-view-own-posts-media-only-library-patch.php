373d18a54976b65d95071372e1e6d3a57bb88ef8
dianjuar <dianjuar@gmail.com>
2017-01-25 00:55:43 -0400

    Big Bang
    Basic plugin configuration

---
+++ includes/class-view-own-posts-media-only-library-patch.php
@@ -0,0 +1,44 @@
+<?php
+
+
+if( !class_exists('View_Own_Posts_Media_Only_Patch') )
+{
+
+/**
+* Class to put a patch to the problem with the custom roles on the plugin 'View own posts and media library items only'
+*
+* The plugin doesn't work with custom roles, just doesn't. With this the plugin will work with custom roles.
+*/
+class View_Own_Posts_Media_Only_Patch
+{
+    
+    function __construct()
+    {
+		# I18n (Loads text-domain) or the translations
+		add_action('plugins_loaded', 
+			array(&$this, 'load_text_domain') );
+    }
+
+    /**
+	 * I18n (Loads text-domain) or the translations
+     */
+    public function load_text_domain() 
+    {
+    	load_plugin_textdomain( VOPMO_PATCH, false, VOPMO_PATCH_PLUGIN_DIRNAME . '/languages' );
+    }
+    
+}# End class
+
+}
+
+
+/**
+ <?php
+
+if( is_plugin_active($plugin) ||
+	 )
+
+require_once( WP_PLUGIN_DIR . '/view-own-posts-media-only/view-own-posts-media-only.php' );
+
+
+ */
---
+++ view-own-posts-media-only-PATCH.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+Plugin Name: View own posts and media library items only - PATCH
+Description: The original plugin doesn't work with custom roles. This plugin fix that problems
+Version: 0.1
+Author: Diego Juliao
+Author URI: http://about.me/dianjuar
+Text Domain: view-own-posts-media-only-PATCH
+Domain Path: /languages/
+*/
+
+# Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if ( !class_exists('View_Own_Posts_Media_Only_Patch') ) 
+{	
+	# -------------------------------------  Define Constants ON   -------------------------------------
+    define( 'VOPMO_PATCH_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+    define( 'VOPMO_PATCH_PLUGIN_DIRNAME', plugin_basename(dirname(__FILE__)));
+    define( 'VOPMO_PATCH_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+    define( 'VOPMO_PATCH', 'view-own-posts-media-only-PATCH' );
+	# -------------------------------------  Define Constants OFF   ------------------------------------
+
+	# plugin includes
+	require_once(VOPMO_PATCH_PLUGIN_DIR . '/includes/class-view-own-posts-media-only-library-patch.php');
+	
+	$view_own_posts_media_only_patch = new View_Own_Posts_Media_Only_Patch();
+}
